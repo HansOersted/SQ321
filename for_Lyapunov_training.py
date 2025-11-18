c15922c41843bd755094ca578d91d9cab8cae7f9
HansOersted <lucidshenzhe@gmail.com>
2025-11-19 02:24:27 +0800

    third climb

--- for_Lyapunov_training.py
+++ for_Lyapunov_training.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import numpy as np
 
-user_input_end_time = 10050 # Select the end time for the training data
+user_input_end_time = 20700 # Select the end time for the training data
 
-df = pd.read_csv("tracking_error_dynamics_9600s_to_10050s.csv")
+df = pd.read_csv("tracking_error_dynamics_20400s_to_20700s.csv")
 
 closest_end_time = df["Time (s)"].iloc[(df["Time (s)"] - user_input_end_time).abs().argmin()]
 print(f"Selected end time: {closest_end_time} s")
 
-subset_df = df[(df["Time (s)"] >= 9600) & (df["Time (s)"] <= closest_end_time)].copy()
+subset_df = df[(df["Time (s)"] >= 20400) & (df["Time (s)"] <= closest_end_time)].copy()
 
 e = subset_df[["tracking_error", "tracking_error_derivative"]].to_numpy()
 de = subset_df[["tracking_error_derivative", "tracking_error_second_derivative"]].to_numpy()
 
-output_filename = f"training_data_9600s_to_{int(closest_end_time)}s.npz"
+output_filename = f"training_data_20400s_to_{int(closest_end_time)}s.npz"
 np.savez(output_filename, e=e, de=de)
 
 print(f"Training data saved as: {output_filename}")
--- interested_time.py
+++ interested_time.py
@@ -3,13 +3,13 @@ import pandas as pd
 df = pd.read_csv("altitude_velocity_acceleration_30s.csv")
 df["Time (s)"] = df["Timestamp"] - df["Timestamp"].iloc[0]
 
-# Selected time 9600s ~ 10050s
-df_range = df[(df["Time (s)"] >= 9600) & (df["Time (s)"] <= 10050)].copy()
+# Selected time 20400s ~ 20700s
+df_range = df[(df["Time (s)"] >= 20400) & (df["Time (s)"] <= 20700)].copy()
 
-df_range["tracking_error"] = 33000 - df_range["Altitude"]
+df_range["tracking_error"] = 35000 - df_range["Altitude"]
 df_range["tracking_error_derivative"] = -df_range["V_vertical"]
 df_range["tracking_error_second_derivative"] = -df_range["A_vertical"]
 
 result = df_range[["Time (s)", "tracking_error", "tracking_error_derivative", "tracking_error_second_derivative"]]
 
-result.to_csv("tracking_error_dynamics_9600s_to_10050s.csv", index=False)
+result.to_csv("tracking_error_dynamics_20400s_to_20700s.csv", index=False)
